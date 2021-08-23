9b6b8d6792c9a11aaf52e0d7616a7c6520295612
OlaShabalina <o.shabalina@outlook.com>
2021-08-23 21:52:25 +1000

    Created routes: home, users, schedules by using data provided in data.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const data = require('./data.js');
 
+// Route to home page
+app.get('/', (req, res) => {
+    res.send('Welcome to our schedule website')
+})
+
+// Route to users
+app.get('/users', (req, res) => {
+    res.send(data.users)
+})
+
+// Route to schedules
+app.get('/schedules', (req, res) => {
+    res.send(data.schedules)
+})
 
 
 app.listen(PORT, () => {
-    console.log(`Testing http://localhost:${PORT}`)
+    console.log(`Listening to http://localhost:${PORT}`)
 })
 
