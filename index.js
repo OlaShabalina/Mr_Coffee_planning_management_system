55a7162751298518f5627a12be9abd6524490042
OlaShabalina <o.shabalina@outlook.com>
2021-08-23 22:13:14 +1000

    Create routes to return the information for a given user and thier schedules

--- index.js
+++ index.js
@@ -13,6 +13,32 @@ app.get('/users', (req, res) => {
     res.send(data.users)
 })
 
+// Route for an individual user
+app.get('/users/:id', (req, res) => {
+    const users = data.users;
+    const { id } = req.params;
+    for (let i = 0; i < users.length; i++ ) {
+        if (i == id) {
+            res.send(users[i]);
+        } else {
+            res.send(`Apologies. Id "${id}" doesn't exists in our system. Please enter a valid id`)
+        }
+    }
+})
+
+// Route for an individual user schedule
+
+app.get('/users/:id/schedules', (req, res) => {
+    let schedules = data.schedules;
+    const { id } = req.params;
+    schedules = schedules.filter(schedule => schedule.user_id == id);
+    if (schedules.user_id) {
+        res.send(schedules);
+    } else {
+        res.send(`User ${id} either doesn't exists or doesn't have a schedule yet`);
+    }
+})
+
 // Route to schedules
 app.get('/schedules', (req, res) => {
     res.send(data.schedules)
