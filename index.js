583f5f38774e1ca22c43bbb1c2b9e87c8c5a6be6
OlaShabalina <o.shabalina@outlook.com>
2021-08-24 21:05:32 +1000

    Introduced Password encryption for registering new user. Added validation for new schedule creation (to check if the user exists before their schedule can be created)

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const data = require('./data.js');
+const data = require('./data.js'); // Access to our in-file database
+const bcrypt = require('bcrypt'); // Password encryption
 
-
+// formating for the data
 app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
+
+
+// taking data out of the file data.js
+const users = data.users;
+const schedules = data.schedules;
 
 // Route to home page
 app.get('/', (req, res) => {
@@ -17,55 +23,57 @@ app.get('/users', (req, res) => {
     res.send(data.users);
 })
 
+// Add a new user
+
+app.post('/users', (req, res) => {
+    let { firstname, lastname, email, password } = req.body;
+    // Password encryption
+    bcrypt.hash(password, 10, (err, hash) => {
+        password = hash;
+        users.push({firstname, lastname, email, password});
+    });
+    res.redirect('/users');
+});
+
+// Add a new user schedule
+
+app.post('/schedules', (req, res) => {
+    const { user_id, day, start_at, end_at } = req.body;
+    if (user_id < users.length) {
+        schedules.push({ user_id, day, start_at, end_at });
+        res.redirect('/schedules');
+    } else {
+        res.send(`User ${user_id} doesn't exists`);
+    }
+});
+
 // Route for an individual user
 app.get('/users/:id', (req, res) => {
-    const users = data.users;
     const { id } = req.params;
     for (let i = 0; i < users.length; i++ ) {
         if (i == id) {
             res.send(users[i]);
-        } else {
-            res.send(`Apologies. Id "${id}" doesn't exists in our system. Please enter a valid id`)
-        }
+        };
     }
 })
 
 // Route for an individual user schedule
 
 app.get('/users/:id/schedules', (req, res) => {
-    let schedules = data.schedules;
     const { id } = req.params;
-    schedules = schedules.filter(schedule => schedule.user_id == id);
-    if (schedules.user_id) {
-        res.send(schedules);
-    } else {
-        res.send(`User ${id} either doesn't exists or doesn't have a schedule yet`);
+    const schedulesShown = [];
+    for ( let schedule of schedules) {
+        if (schedule.user_id == id) {
+            schedulesShown.push(schedule);
+        }
     }
-})
+    res.send(schedulesShown);
+});
 
 // Route to schedules
 app.get('/schedules', (req, res) => {
     res.send(data.schedules)
 })
-
-// Add a new user
-
-app.post('/users', (req, res) => {
-    const users = data.users;
-    const post = req.body;
-    users.push(post);
-    res.redirect('/users')
-});
-
-// Add a new user schedule
-
-app.post('/schedules', (req, res) => {
-    const schedules = data.schedules;
-    const post = req.body;
-    schedules.push(post);
-    res.redirect('/schedules')
-});
-
 
 app.listen(PORT, () => {
     console.log(`Listening to http://localhost:${PORT}`)
